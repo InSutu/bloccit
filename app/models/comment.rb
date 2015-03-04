c800e537de86e14f3b27081a5e2f6a2d8c74c1d1
InSutu <dpauli@insutu.com>
2015-03-04 15:18:59 -0800

    another-interlude

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -12,8 +12,18 @@ class Comment < ActiveRecord::Base
  
    def send_favorite_emails
      post.favorites.each do |favorite|
-       FavoriteMailer.new_comment(favorite.user, post, self).deliver
+        # FavoriteMailer.new_comment(favorite.user, post, self).
+        # if favorite.user_id != self.user_id && favorite.user.email_favorites?
+        if should_receive_update_for?(favorite)
+
+        FavoriteMailer.new_comment(favorite.user, self.post, self).deliver
+       end
      end
    end
+
+  def should_receive_update_for?(favorite)
+     user_id != favorite.user_id && favorite.user.email_favorites?
+  end
+
 end
 # Comments checkpoint:  https://github.com/Bloc/rails-tutorial/blob/master/answers.md
