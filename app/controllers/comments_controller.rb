e04d2bd624a6c17dbe4e1357509f92af13edc593
InSutu <dpauli@insutu.com>
2015-03-07 06:53:42 -0800

    Creating Ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,47 @@
 class CommentsController < ApplicationController
-  def create
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
+  
+def create
+    @post = Post.find(params[:post_id])
     @comments = @post.comments
 
-    @comment = current_user.comments.build(comment_param)
+    @comment = current_user.comments.build( comment_params )
     @comment.post = @post
+    @new_comment = Comment.new
 
-    #authorize! :create, @comment, message: "You need to be signed in to do that."
-     authorize @comment
-      if @comment.save
-        #flash[:notice] = "Comment was created."
-        redirect_to [@topic, @post], notice: "Comment was created."
+    authorize @comment
 
-      else
-        flash[:error] = "There was an error saving your post."
-        render :new
-      end
+    if @comment.save
+      flash[:notice] = "Comment was created."
+    else
+      flash[:error] = "There was an error saving the comment. Please try again."
     end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+# REPLACED BY CREATING WITH AJAX CHECKPOINT
+  # def create
+  #   @topic = Topic.find(params[:topic_id])
+  #   @post = @topic.posts.find(params[:post_id])
+  #   @comments = @post.comments
+
+  #   @comment = current_user.comments.build(comment_param)
+  #   @comment.post = @post
+
+  #   #authorize! :create, @comment, message: "You need to be signed in to do that."
+  #    authorize @comment
+  #     if @comment.save
+  #       #flash[:notice] = "Comment was created."
+  #       redirect_to [@topic, @post], notice: "Comment was created."
+
+  #     else
+  #       flash[:error] = "There was an error saving your post."
+  #       render :new
+  #     end
+  #   end
 
 
 
