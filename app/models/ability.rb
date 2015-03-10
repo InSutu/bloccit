13afee636d42e66cd6879b162d34952aaf26d9e2
InSutu <dpauli@insutu.com>
2015-03-09 19:04:26 -0700

    Prod TShoot ability.rb

--- app/models/ability.rb
+++
@@ -1,6 +0,0 @@
-#ability.rb
-# From Comments checkpoint:  https://github.com/Bloc/rails-tutorial/blob/master/answers.md
-   if user.role? :member
-      can :manage, Post, :user_id => user.id
-      can :manage, Comment, :user_id => user.id
-    end
